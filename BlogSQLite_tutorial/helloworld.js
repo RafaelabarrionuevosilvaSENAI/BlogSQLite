01c8852977ad0266c3aba92d41891522c864365f
RafaelabarrionuevosilvaSENAI <yae.miko.lindona.mito@gmail.com>
2025-03-14 13:29:23 -0300

    Ola senai

--- BlogSQLite_tutorial/helloworld.js
+++ BlogSQLite_tutorial/helloworld.js
@@ -4,8 +4,23 @@ const PORT = 8000;
 
 const app = express();
 
+const index = "<a href=/'/sobre'>Sobre</a><a href='/info'>Info</a>"
+
+
+app.get("/", (req, res) => {
+res.send(index);
+});
+
 app.get('/', (req, res) => {
     res.send("olá SESI!");
+});
+
+app.get("/sobre", (req, res) => {
+    res.send("Vc está na página Sobre");
+});
+
+app.get("/info", (req, res) => {
+    res.send("Vc está na página info");
 });
 
 app.listen(PORT, () => {
